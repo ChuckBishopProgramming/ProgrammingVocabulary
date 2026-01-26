dc186dd12caaede65e7b73cf7f46f66a1fa88351
Chuck Bishop <chuckbishopprogamming@gmail.com>
2026-01-25 23:36:04 -0400

    Initial notes and git ignore

---
+++ VocabularySandbox/VocaBox/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace VocaBox
+{
+    // GOAL:
+    // keep track of integral vocabulary
+    // provide examples where possible
+
+    //FORMAT:
+    //++ Proper definition
+    //-- Personal definition
+    //?? Guess at definition
+
+    //PURPOSE:
+    // Ensure we have the skills to search and communicate vital information
+    // Test our existing knowledge
+    // Practice skills
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
