a8a08bc8826b18a888cbbb8c4276912b8642300e
Chuck Bishop <chuckbishopprogamming@gmail.com>
2026-01-25 23:58:09 -0400

    attribute

--- VocabularySandbox/VocaBox/Program.cs
+++ VocabularySandbox/VocaBox/Program.cs
@@ -7,11 +7,12 @@
     //FORMAT:
     // string name = Proper definition
     // string name++ = Personal or embellished definition
-    // string name?? =  Guess at definition
-    // string nameDEF = protected word work around
+    // string nameGUESS =  Guess at definition
+    // string nameQUESTION = Question to look up
+    // string nameDEF = Protected word work around
 
     //PURPOSE:
-    // Ensure we have the skills to search and communicate vital information
+    // Ensure we have the skills to search, naviagate and communicate vital information
     // Test our existing knowledge
     // Practice skills
     internal class Program       
@@ -21,8 +22,16 @@
             string objectOrientedProgramming = "A style of programming that focuses on objects";
             string objectOrientedProgramming++ = "object focused, less limited than procedural but a little more complicated. Translates to visual thought processes fairly well";
 
-            string objectDef = "An object is an instance of a class. Needs the new keyword";
+            string objectDEF = "An object is an instance of a class. Needs the new keyword";
             string instance = "A specific occurance of a class";
+
+            string programming = "giving machines precise step by step instructions to carry out various tasks";
+
+            string classDEF = "the template for a classes behaviors and attributes";
+
+            string attribute = "A form of metadata.";
+            string attributeQUESTION = "Question: How is an attribute different than a property? ";            
+            string attributeANSWER = "field and property are part of the object's state and behavior, ATTRIBUTE describes the code itself. Not the object.";
 
             Console.WriteLine("Hello, World!");
         }
