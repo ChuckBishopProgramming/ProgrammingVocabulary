5ab1298c970d4ddbfd8c283b9c0bb22e86342219
Chuck Bishop <chuckbishopprogamming@gmail.com>
2026-01-25 23:45:47 -0400

    OOP, object, instance

--- VocabularySandbox/VocaBox/Program.cs
+++ VocabularySandbox/VocaBox/Program.cs
@@ -5,18 +5,25 @@
     // provide examples where possible
 
     //FORMAT:
-    //++ Proper definition
-    //-- Personal definition
-    //?? Guess at definition
+    // string name = Proper definition
+    // string name++ = Personal or embellished definition
+    // string name?? =  Guess at definition
+    // string nameDEF = protected word work around
 
     //PURPOSE:
     // Ensure we have the skills to search and communicate vital information
     // Test our existing knowledge
     // Practice skills
-    internal class Program
+    internal class Program       
     {
         static void Main(string[] args)
         {
+            string objectOrientedProgramming = "A style of programming that focuses on objects";
+            string objectOrientedProgramming++ = "object focused, less limited than procedural but a little more complicated. Translates to visual thought processes fairly well";
+
+            string objectDef = "An object is an instance of a class. Needs the new keyword";
+            string instance = "A specific occurance of a class";
+
             Console.WriteLine("Hello, World!");
         }
     }
